f5868f0697cea0775c26512cdc825ece35075122
BubyrYehorDev <bubyryehordev@gmail.com>
2022-08-16 13:50:18 +0300

    create product item widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,27 +14,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Shopy'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(child: Text('Welcome to Shopy app!')),
+      routes: {
+        '/': (context) => ProductsOverviewScreen(),
+        // CategoryMealsScreen.routeName: (context) => const CategoryMealsScreen(),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => ProductsOverviewScreen());
+      },
     );
   }
 }
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,57 +1,88 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
+import '../widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = [
     Product(
       id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
+      title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+      description:
+          'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
       price: 29.99,
       discountPercentage: 10,
-      brand: 'Jack & Jonson',
-      category: 'Clothes',
+      brand: 'Fjallraven',
+      category: ProductCategories.ForMen,
       rating: 10,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+      imageUrl: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
     ),
     Product(
       id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
+      title: 'Mens Casual Premium Slim Fit T-Shirts',
+      description:
+          'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
       price: 59.99,
       discountPercentage: 10,
       brand: 'Jack & Jonson',
-      category: 'Clothes',
+      category: ProductCategories.ForMen,
       rating: 10,
       imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+          'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
     ),
     Product(
       id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
+      title: 'Mens Cotton Jacket',
+      description:
+          'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
       price: 19.99,
       discountPercentage: 10,
-      brand: 'Jack & Jonson',
-      category: 'Clothes',
+      brand: 'Armani',
+      category: ProductCategories.ForMen,
       rating: 10,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+      imageUrl: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
     ),
     Product(
       id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
+      title: 'Mens Casual Slim Fit',
+      description:
+          'The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.',
       price: 49.99,
       discountPercentage: 10,
-      brand: 'Jack & Jonson',
-      category: 'Clothes',
+      brand: 'H&M',
+      category: ProductCategories.ForMen,
+      rating: 10,
+      imageUrl: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    ),
+    Product(
+      id: 'p5',
+      title:
+          'John Hardy Women\'s Legends Naga Gold & Silver Dragon Station Chain Bracelet',
+      description:
+          'From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean\'s pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.',
+      price: 49.99,
+      discountPercentage: 10,
+      brand: 'H&M',
+      category: ProductCategories.Jewelery,
       rating: 10,
       imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+          'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
+    ),
+    Product(
+      id: 'p6',
+      title: 'Solid Gold Petite Micropave',
+      description:
+          'Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.',
+      price: 49.99,
+      discountPercentage: 10,
+      brand: 'Gold century',
+      category: ProductCategories.Jewelery,
+      rating: 10,
+      imageUrl:
+          'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg',
     ),
   ];
+
+  ProductsOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -60,8 +91,10 @@ class ProductsOverviewScreen extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.all(10.0),
         itemCount: loadedProducts.length,
-        itemBuilder: (context, index) => Container(
-          child: Text('product $index'),
+        itemBuilder: (context, index) => ProductItem(
+          loadedProducts[index].id,
+          loadedProducts[index].title,
+          loadedProducts[index].imageUrl,
         ),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
