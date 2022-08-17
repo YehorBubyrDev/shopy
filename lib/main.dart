645ce14c9615b0259c8907dbeffe43c621460f5d
BubyrYehorDev <bubyryehordev@gmail.com>
2022-08-17 13:41:52 +0300

    delete all providers

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
-import './providers/test_pizza_provider.dart';
 import './providers/products_provider/products.dart';
 
 void main() {
@@ -15,7 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => PizzaProvider()),
         ChangeNotifierProvider(create: (context) => ProductsProvider()),
       ],
       child: MaterialApp(
