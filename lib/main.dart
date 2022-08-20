7a50c846581539bc463d1de3a05f7b5cfa796271
BubyrYehorDev <bubyryehordev@gmail.com>
2022-08-20 14:50:21 +0300

    code refactor of providing

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider.value(value: ProductsProvider()),
+        ChangeNotifierProvider(create: (context) => ProductsProvider())
       ],
       child: MaterialApp(
         theme: ThemeData(
