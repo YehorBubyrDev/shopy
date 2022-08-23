ab90d90f028f18bb82aa9912af98c42d3dc1ad61
BubyrYehorDev <bubyryehordev@gmail.com>
2022-08-23 15:51:04 +0300

    add inputs validation

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -27,76 +27,122 @@ class ProductItem extends StatelessWidget {
           footer: GridTileBar(
             backgroundColor: Colors.black87,
             leading: Consumer<Product>(
-              builder: (context, value, child) => IconButton(
-                  icon: product.isFavorite
-                      ? const Icon(
-                          Icons.favorite,
-                          color: Colors.pink,
-                        )
-                      : const Icon(Icons.favorite_outline),
-                  onPressed: (() => {
-                        product.toggleFavoriteStatus(),
-                        ScaffoldMessenger.of(context).hideCurrentSnackBar(),
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: const Text(
-                            'Added to favorite!',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          action: SnackBarAction(
-                              textColor: Colors.white,
-                              label: 'UNDO',
-                              onPressed: () {
-                                product.toggleFavoriteStatus();
-                              }),
-                          backgroundColor: Colors.pink,
-                          behavior: SnackBarBehavior.floating,
-                          duration: const Duration(milliseconds: 2000),
-                        ))
-                      }),
-                  color: Theme.of(context).colorScheme.secondary),
+              builder: (context, value, child) =>
+                  FavoriteButton(product: product),
             ),
             title: Text(
               '\$${product.price.toString()}',
               textAlign: TextAlign.center,
             ),
             trailing: Consumer<Product>(
-              builder: (context, value, child) => IconButton(
-                icon: const Icon(Icons.shopping_cart),
-                onPressed: (() => {
-                      cart.addItem(product.id, product.title, product.price),
-                      ScaffoldMessenger.of(context).hideCurrentSnackBar(),
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: const Text(
-                          'Added to cart!',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        action: SnackBarAction(
-                            textColor: Colors.black,
-                            label: 'UNDO',
-                            onPressed: () {
-                              cart.removeItem(product.id);
-                            }),
-                        backgroundColor: Colors.cyanAccent,
-                        behavior: SnackBarBehavior.floating,
-                        duration: const Duration(milliseconds: 2000),
-                      ))
-                    }),
-                color: Theme.of(context).colorScheme.secondary,
-              ),
+              builder: (context, value, child) =>
+                  CartButton(cart: cart, product: product),
             ),
           ),
           child: Image.network(
             product.imageUrl,
             fit: BoxFit.cover,
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              );
+            },
           ),
         ),
       ),
     );
+  }
+}
+
+class CartButton extends StatelessWidget {
+  const CartButton({
+    Key? key,
+    required this.cart,
+    required this.product,
+  }) : super(key: key);
+
+  final CartProvider cart;
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.shopping_cart),
+      onPressed: (() => {
+            cart.addItem(product.id, product.title, product.price),
+            ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: const Text(
+                'Added to cart!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              action: SnackBarAction(
+                  textColor: Colors.black,
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeItem(product.id);
+                  }),
+              backgroundColor: Colors.cyanAccent,
+              behavior: SnackBarBehavior.floating,
+              duration: const Duration(milliseconds: 2000),
+            ))
+          }),
+      color: Theme.of(context).colorScheme.secondary,
+    );
+  }
+}
+
+class FavoriteButton extends StatelessWidget {
+  const FavoriteButton({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: product.isFavorite
+            ? const Icon(
+                Icons.favorite,
+                color: Colors.pink,
+              )
+            : const Icon(Icons.favorite_outline),
+        onPressed: (() => product.isFavorite
+            ? {product.toggleFavoriteStatus()}
+            : {
+                product.toggleFavoriteStatus(),
+                ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: const Text(
+                    'Added to favorite!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  action: SnackBarAction(
+                      textColor: Colors.white,
+                      label: 'UNDO',
+                      onPressed: () {
+                        product.toggleFavoriteStatus();
+                      }),
+                  backgroundColor: Colors.pink,
+                  behavior: SnackBarBehavior.floating,
+                  duration: const Duration(milliseconds: 2000),
+                ))
+              }),
+        color: Theme.of(context).colorScheme.secondary);
   }
 }
